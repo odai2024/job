e001e1e83ddd86f6376f05a6c6ccfc0f2e369b10
odai2024 <odai2015100@gmail.com>
2024-09-15 19:07:32 +0300

    Add files via upload

---
+++ function/script.js
@@ -0,0 +1,53 @@
+function names(){
+console.log("odai")
+console.log("marwan")
+}
+names()
+
+function add(x, y){ 
+console.log(x+y)
+}
+add(1, 1)
+add(5, 5)
+
+function less(x, y){ 
+    console.log(x-y)
+}
+less(1, 1)
+less(5, 5)
+
+function multi(x, y){      
+console.log(x*y)
+}
+multi(1, 1)
+multi(5, 5)
+
+function divide(x, y){
+console.log(x/y)
+}
+divide(10, 5)
+divide(9, 3)
+
+function welcome(name){
+console.log("welcome",name)
+}
+welcome("Ahmed")
+welcome("Odai")
+
+function area_of_square(side){
+console.log(side * side)
+}
+area_of_square(5)
+area_of_square(1)
+
+function area_of_rectangle(legnth , width){
+console.log(legnth * width)
+}
+area_of_rectangle(5,4)
+area_of_rectangle(4,1)
+
+function perimeter_rectangle(l,w){ 
+console.log(2*(l+w))
+}
+perimeter_rectangle(5,2)
+perimeter_rectangle(4,1)
